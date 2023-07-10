6c7df600e5c2150face47b48cbd45d40640e6794
HL <hoanglinh171038@gmail.com>
2023-07-10 11:28:39 +0700

    9. use param to style category item

--- app/components/navbar/Categories.tsx
+++ app/components/navbar/Categories.tsx
@@ -35,69 +35,80 @@ export const categories = [
       icon: MdOutlineVilla,
       description: 'This property is modern!'
     },
-    // {
-    //   label: 'Countryside',
-    //   icon: TbMountain,
-    //   description: 'This property is in the countryside!'
-    // },
-    // {
-    //   label: 'Pools',
-    //   icon: TbPool,
-    //   description: 'This is property has a beautiful pool!'
-    // },
-    // {
-    //   label: 'Islands',
-    //   icon: GiIsland,
-    //   description: 'This property is on an island!'
-    // },
-    // {
-    //   label: 'Lake',
-    //   icon: GiBoatFishing,
-    //   description: 'This property is near a lake!'
-    // },
-    // {
-    //   label: 'Skiing',
-    //   icon: FaSkiing,
-    //   description: 'This property has skiing activies!'
-    // },
-    // {
-    //   label: 'Castles',
-    //   icon: GiCastle,
-    //   description: 'This property is an ancient castle!'
-    // },
-    // {
-    //   label: 'Caves',
-    //   icon: GiCaveEntrance,
-    //   description: 'This property is in a spooky cave!'
-    // },
-    // {
-    //   label: 'Camping',
-    //   icon: GiForestCamp,
-    //   description: 'This property offers camping activities!'
-    // },
-    // {
-    //   label: 'Arctic',
-    //   icon: BsSnow,
-    //   description: 'This property is in arctic environment!'
-    // },
-    // {
-    //   label: 'Desert',
-    //   icon: GiCactus,
-    //   description: 'This property is in the desert!'
-    // },
-    // {
-    //   label: 'Barns',
-    //   icon: GiBarn,
-    //   description: 'This property is in a barn!'
-    // },
-    // {
-    //   label: 'Lux',
-    //   icon: IoDiamond,
-    //   description: 'This property is brand new and luxurious!'
-    // }
+    {
+      label: 'Countryside',
+      icon: TbMountain,
+      description: 'This property is in the countryside!'
+    },
+    {
+      label: 'Pools',
+      icon: TbPool,
+      description: 'This is property has a beautiful pool!'
+    },
+    {
+      label: 'Islands',
+      icon: GiIsland,
+      description: 'This property is on an island!'
+    },
+    {
+      label: 'Lake',
+      icon: GiBoatFishing,
+      description: 'This property is near a lake!'
+    },
+    {
+      label: 'Skiing',
+      icon: FaSkiing,
+      description: 'This property has skiing activies!'
+    },
+    {
+      label: 'Castles',
+      icon: GiCastle,
+      description: 'This property is an ancient castle!'
+    },
+    {
+      label: 'Caves',
+      icon: GiCaveEntrance,
+      description: 'This property is in a spooky cave!'
+    },
+    {
+      label: 'Camping',
+      icon: GiForestCamp,
+      description: 'This property offers camping activities!'
+    },
+    {
+      label: 'Arctic',
+      icon: BsSnow,
+      description: 'This property is in arctic environment!'
+    },
+    {
+      label: 'Desert',
+      icon: GiCactus,
+      description: 'This property is in the desert!'
+    },
+    {
+      label: 'Barns',
+      icon: GiBarn,
+      description: 'This property is in a barn!'
+    },
+    {
+      label: 'Lux',
+      icon: IoDiamond,
+      description: 'This property is brand new and luxurious!'
+    }
   ]
 
 const Categories = () =>{
+  const params = useSearchParams();// take parsms form url
+  const pathname = usePathname();// take path name ex http://localhost:3000/ --> pathname='/'
+  const category = params?.get('category') // take params(category  in url) = category
+
+  // check if in home pages ('/')
+  const ismainPage = pathname === '/';
+
+  if(!ismainPage){
+    return null;
+  }
+
     return (
         <Container>
             <div
@@ -114,6 +125,8 @@ const Categories = () =>{
                 return <CategoryBox
                         key={item.label}
                         label={item.label}
+                        // category from params === current label --> true else false 
+                        selected={category ===item.label}
                         description={item.description}
                         icon={item.icon}
                       />
