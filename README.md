68c2fb5cf2ddaaeb5fffeac0e2286c20588f51c4
HL <hoanglinh171038@gmail.com>
2023-07-20 09:59:11 +0700

    28.show data to search bar

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-27.loading and error page
+28.show data to search bar
+    - useSearchParam to take params from link
+    - getByValue --> to take label country
+    - get info data
+    - check and take country label , day, number of guest 
+    - show
 
         
 
--- app/components/navbar/Search.tsx
+++ app/components/navbar/Search.tsx
@@ -1,11 +1,59 @@
 'use client'
 
+import useCountries from '@/app/hooks/useCountries';
 import useSearchModel from '@/app/hooks/useSearchModel'
+import { differenceInDays } from 'date-fns';
+import { useSearchParams } from 'next/navigation';
+import { useMemo } from 'react';
 import {BiSearch} from 'react-icons/bi'
 
 const Search =()=>{
 
-    const searchModal = useSearchModel()
+    const searchModal = useSearchModel();
+    const params = useSearchParams();
+    const { getByValue} = useCountries();
+
+    const locationValue = params?.get('locationValue');
+    const startDate = params?.get('startDate');
+    const endDate = params?.get('endDate');
+    const guestCount = params?.get('guestCount');
+
+    // check location and take label
+    const locationsLabel = useMemo(()=>{
+        if(locationValue){
+            return getByValue(locationValue as string)?.label;
+        }
+
+        return 'Anywhere';
+    },[locationValue, getByValue])
+
+    //check and take day
+    const durationLabel = useMemo(() => {
+        if (startDate && endDate)
+        {
+            const start = new Date(startDate as string);
+            const end = new Date(endDate as string);
+            let diff = differenceInDays(end,start);
+
+            if(diff === 0){
+                diff =1
+            }
+
+            return `${diff} Days`;
+        }
+
+        return 'Any Week'
+    }, [startDate,endDate])
+
+    //check and take guest
+
+    const guestLabel = useMemo(() => {
+        if(guestCount) {
+            return `${guestCount} Guests`;
+        }
+
+        return "Any Guests"
+    },[guestCount])
  return (
     <div 
         onClick={searchModal.onOpen}
@@ -36,7 +84,7 @@ const Search =()=>{
                     px-6
                 "
             >
-                Anywhere
+               {locationsLabel}
             </div>
             <div
                 className="
@@ -50,7 +98,7 @@ const Search =()=>{
                     text-center
                 "
             >
-                Any Week
+            {durationLabel}
             </div>
             <div
                 className="
@@ -64,7 +112,7 @@ const Search =()=>{
                     gap-3
                 "
             >   
-                <div className="hidden sm:block">Add Guest</div>
+                <div className="hidden sm:block">{guestLabel}</div>
                 <div
                     className="
                         p-2
