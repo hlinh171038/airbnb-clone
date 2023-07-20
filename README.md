c17e1d259845bf9f5339f70ecd289d742b042848
HL <hoanglinh171038@gmail.com>
2023-07-20 09:31:00 +0700

    27.loading and error page

--- README.md
+++ README.md
@@ -1,7 +1,4 @@
-26.search filter
-    - create hook for search modal
-    - create searchModal.tsx
-    - modify listing to search by params
+27.loading and error page
 
         
 
--- app/hooks/useSearchModel.ts
+++ app/hooks/useSearchModel.ts
@@ -7,7 +7,7 @@ interface SearchModelProps {
 }
 
 const useSearchModel = create<SearchModelProps>((set)=>({
-    isOpen: true,
+    isOpen: false,
     onOpen: () =>set({isOpen: true}),
     onClose: () =>set({isOpen: false})
 })) ;
